7278dc4f0fbbaca6fe9da6836c0e2800d8c72fb8
Thomas Doyle <tmdoyle11@gmail.com>
2022-02-17 13:48:45 -0600

    commit tree ccb843a00310e5199de4c8fbf525fb075b208918

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+    "Hello!".toUpperCase(); // 'HELLO!
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    "Hello!".toLowerCase(); // 'hello!
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === "Let\'s have dinner together!") {
+        return "I would love to!";
+    } else if (string === string.toUpperCase()) {
+        return "YES INDEED!";
+    } else if (string === string.toLowerCase()) {
+        return "I can\'t hear you!"
+    }
+}
